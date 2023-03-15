7af70d767e643012ee00a8db48bff7b2d33c370d
Jesus Lara <jesuslarag@gmail.com>
2023-03-15 01:16:52 +0100

    New project QuerySource

--- querysource/__init__.py
+++ querysource/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 """
-QuerySource for Navigator.
+QuerySource.
 
 Querysource is a tool for querying database with different syntax, no matter
 type of database (SQL, noSQL, etc).
 """
 
 ## more information
-__copyright__ = 'Copyright (c) 2020-2022 Jesus Lara'
+__copyright__ = 'Copyright (c) 2020-2023 Jesus Lara'
 __license__ = 'BSD'
 __author__ = 'Jesus Lara <jesuslarag@gmail.com>'
