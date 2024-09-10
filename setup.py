36f84681548d858e6c128f9fb4d74511782bf8d4
Jesus Lara <jesuslarag@gmail.com>
2024-09-10 18:51:56 +0200

    fix httpx

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ setup(
     ],
     install_requires=[
         'LivePopularTimes==1.3',
-        'httpx>=0.25.0,<0.27.0',
+        'httpx>=0.25.0,<=0.27.0',
         'hubspot-api-client==7.5.0',
         'oauth2client==4.1.3',
         'google-analytics-data==0.16.2',
