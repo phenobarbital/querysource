82beab5dceb56c0f10cdc17d828b9dabe515648a
Jesus Lara <jesuslarag@gmail.com>
2025-03-13 15:55:17 +0100

    wip: documentDB provider

--- querysource/providers/documentdb.py
+++ querysource/providers/documentdb.py
@@ -58,14 +58,14 @@ class documentdbProvider(BaseProvider):
         if qstype == 'slug':
             if self._definition.is_raw is True:
                 self.is_raw = True
-                self._query = self._definition.query_raw
-                print(f"= Query is:: {self._query}")
+                self._query = self.get_raw_query(self._definition.query_raw)
+                self._logger.notice(f"= Query is:: {self._query}")
         else:
             self._query = kwargs['query_raw']
             if kwargs.get('raw_query', False):
                 try:
                     self._query = self.get_raw_query(self._query)
-                    self._logger.debug(f"= Raw Query: {self._query}")
+                    self._logger.notice(f"= RAW Query: {self._query}")
                 except Exception as err:
                     raise DriverError(
                         f'Mongo Query Error: {err}'
@@ -82,17 +82,25 @@ class documentdbProvider(BaseProvider):
             dict: Processed MongoDB query object
         """
         if self._conditions:
-            return query.format_map(
-                defaultdict(str, SafeDict(**self._conditions))
-            )
-        conditions = {**self.replacement}
-        query = query.format_map(
-            defaultdict(str, SafeDict(**conditions))
-        )
+            try:
+                return query.format_map(
+                    defaultdict(str, SafeDict(**self._conditions))
+                )
+            except ValueError:
+                pass
         # Parse back into dictionary
         try:
-            return json_decoder(query)
-        except JSONParserError:
+            qry = json_decoder(query)
+            _filter = qry.pop('filter', {})
+            try:
+                qry['query'].update(_filter)
+            except KeyError:
+                qry['query'] = _filter
+            return qry
+        except JSONParserError as ex:
+            self._logger.error(
+                f"Error Parsing Query: {ex}"
+            )
             return query
 
     async def prepare_connection(self):
@@ -102,6 +110,7 @@ class documentdbProvider(BaseProvider):
         This method will use the parser to build the MongoDB query
         from the JSON definition if needed.
         """
+        await super(documentdbProvider, self).prepare_connection()
         if not self._connection:
             # TODO: get a new connection
             raise DriverError(
@@ -161,26 +170,18 @@ class documentdbProvider(BaseProvider):
         error = None
         try:
             error = None
-            print('QUERY > ', self._connection)
             async with await self._connection.connection() as conn:
                 await conn.use(self._database)
-                # result, error = await conn.query(
-                #     **self._query
-                # )
-                query = {'limit': 1}
-                query = {'race_name': 'Breitenberg, Kihn and Walter Death March'}
                 result, error = await conn.query(
-                    collection_name='races',
-                    query=query,
+                    **self._query
                 )
-                print('RESULT > ', result)
                 if error:
                     return [None, error]
                 if result:
                     self._result = result
                 else:
                     raise DataNotFound(
-                        'Empty Result'
+                        f'Empty Result for {self._query!r}'
                     )
                 return [result, error]
         except StatementError as err:
