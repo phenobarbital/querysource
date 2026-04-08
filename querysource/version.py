3b71f71446f2496a668872ac8044814ee41b58a3
Jesus Lara <jesuslarag@gmail.com>
2026-04-08 22:24:22 +0200

    bump version

--- querysource/version.py
+++ querysource/version.py
@@ -6,7 +6,7 @@ __description__ = (
     'QuerySource Query parser and generator.'
     'Supports SQL, NoSQL, MongoDB, Cassandra, Elasticsearch, etc.'
 )
-__version__ = '4.1.2'
+__version__ = '4.1.3'
 __url__ = 'https://github.com/phenobarbital/querysource'
 __copyright__ = 'Copyright (c) 2018-2025 Jesus Lara'
 __author__ = 'Jesus Lara'
