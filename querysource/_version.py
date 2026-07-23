00208f0f55777957e45fe1b4f72600245c8303a9
Jesus <jesuslarag@gmail.com>
2026-07-23 18:05:03 +0200

    pyproject updates

--- querysource/_version.py
+++ querysource/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '4.5.3.dev8+gbe9c7f0e5.d20260630'
-__version_tuple__ = version_tuple = (4, 5, 3, 'dev8', 'gbe9c7f0e5.d20260630')
+__version__ = version = '4.5.9.dev7+g3eba7b663.d20260723'
+__version_tuple__ = version_tuple = (4, 5, 9, 'dev7', 'g3eba7b663.d20260723')
 
-__commit_id__ = commit_id = 'gbe9c7f0e5'
+__commit_id__ = commit_id = 'g3eba7b663'
